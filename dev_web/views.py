f4d4b3921ccd781b58a427450f10858ad18d95c4
Jeff Lai <35415927+jlai3214@users.noreply.github.com>
2019-02-21 21:52:15 -0800

    ''

--- dev_web/views.py
+++ dev_web/views.py
@@ -6,5 +6,7 @@ from django.shortcuts import render, get_object_or_404
 
 def home(request):
     # return HttpResponse("SQAInsider Home Page")
-    return render(request, 'movies/index.html', {'movies': movies})
+    # return render(request, 'movies/index.html', {'movies': movies})
+    return render(request, 'movies/index.html', )
+
     # return render(request, 'home.html', )
