99b4e6de10f41263912e772e4889ff054e336866
Jeff Lai <35415927+jlai3214@users.noreply.github.com>
2019-02-24 22:32:46 -0800

    updated

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BookAPIView
 
 urlpatterns = [
-    path('rest/books', BookAPIView.as_view()),
+    path('rest/books', BookAPIView.as_view(), name='api_books'),
 
 
 ]
